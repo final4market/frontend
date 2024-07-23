37fa0e88b0a1752c55dac77044318a746bd32f36
juanalee <optihyuny07@gmail.com>
2024-07-23 17:37:06 +0900

    tempchange

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
+import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import AdminMembers from './admin/components/adminMembers/AdminMembers';
@@ -8,39 +7,33 @@ import LoginForm from './components/auth/LoginForm';
 import ProductRegistration from './components/productRegistration/ProductRegistration';
 import MypageProductSalesList from './components/myPage/MypageProductSalesList';
 import MypageProductPurchaseHistory from './components/myPage/MypageProductPurchaseHistory';
-import SubMain from './components/subPage/Sub_main';
-import ProtectedRoute from './admin/components/ProtectedRoute';
-import './services/AxiosSetup'; 
-=======
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import MyStore from './components/mypage/MyStore';
 import MyInfo from './components/mypage/MyInfo';
 import InterestProduct from './components/mypage/InterestProduct';
 import ReceivedReview from './components/mypage/ReceivedReview';
->>>>>>> 1f7764110cb9a2fd5ed1bd561f2f4d261fd97a66
+import SubMain from './components/subPage/Sub_main';
+import ProtectedRoute from './admin/components/ProtectedRoute';
+import './services/AxiosSetup';
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
+
         <Route path="/login" element={<LoginForm />} />
         <Route path="/productPage" element={<SubMain/>} />
+        <Route path="/myStore" element={<MyStore/>}/>
+        <Route path="/myInfo" element={<MyInfo/>}/>
         <Route path="/productRegister" element={<ProductRegistration/>} />
         <Route path="/sell-history" element={<MypageProductSalesList />} />
-        <Route path="/buy-history" element={<MypageProductPurchaseHistory />} /> 
+        <Route path="/buy-history" element={<MypageProductPurchaseHistory />} />        
+        <Route path="/interestProduct" element={<InterestProduct/>}/>
+        <Route path="/receivedReview" element={<ReceivedReview/>}/> 
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<Navigate to="/admin/reports" />} />
           <Route path="/admin/members" element={<AdminMembers />} />
           <Route path="/admin/reports" element={<AdminReports />} />
         </Route>
-=======
-        <Route path="/myStore" element={<MyStore/>}/>
-        <Route path="/myInfo" element={<MyInfo/>}/>
-        <Route path="/interestProduct" element={<InterestProduct/>}/>
-        <Route path="/receivedReview" element={<ReceivedReview/>}/>
->>>>>>> 1f7764110cb9a2fd5ed1bd561f2f4d261fd97a66
       </Routes>
     </Router>
   );
